9cd46a50d2a59a46c0b92f4f21f23580cc6ebe11
jonnhygarcia <76399015+jonnhygarcia@users.noreply.github.com>
2025-10-06 19:01:47 +0200

    a ver si sale

--- ejemplos/Entrada_salida.py
+++ ejemplos/Entrada_salida.py
@@ -1,10 +1,10 @@
 import csv
-with open("datos.csv", "w", newline="") as archivo:
+with open("./ejemplos/datos.csv", "w", newline="") as archivo:
     escritor = csv.writer(archivo)
     escritor.writerow(["nombre", "Edad"])
     escritor.writerow(["Ana", 45])
     
-    with open("datos.csv", "r") as archivo:
+    with open("./ejemplos/datos.csv", "r") as archivo:
         lector = csv.reader(archivo)
         for fila in lector:
             print(fila)
