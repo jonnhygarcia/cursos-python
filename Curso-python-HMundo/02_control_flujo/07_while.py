bec4e3b758d300d5a863e20835d434bc62ea0952
jonnhygarcia <76399015+jonnhygarcia@users.noreply.github.com>
2025-10-15 22:47:43 +0200

    ...

--- Curso-python-HMundo/02_control_flujo/07_while.py
+++ Curso-python-HMundo/02_control_flujo/07_while.py
@@ -4,11 +4,11 @@
 #     numero *= 2
 
 # se le da a la variable comando un valor vacio.
-# comando = ""
+comando = ""
 # se inicia un bucle while que continuará hasta que el valor de comando sea "salir" (sin importar mayúsculas o minúsculas).
-# while comando.lower() != "salir": # -> se usa lower() para convertir cualquier texto que se introduzca en minusculas.
-#     comando = input("Ingrese un comando (escriba 'salir' para terminar): ")
-#     print(f"Comando ingresado: {comando}") # -> se imprime el string comando ingresado por el usuario.
+while comando.lower() != "salir": # -> se usa lower() para convertir cualquier texto que se introduzca en minusculas.
+    comando = input("Ingrese un comando (escriba 'salir' para terminar): ")
+    print(f"Comando ingresado: {comando}") # -> se imprime el string comando ingresado por el usuario.
 
 # es importante que cuando se pide datos al uusuiario, este puede escribirlo mal por error, y con el lower() se evita el error.
 
--- Curso-python-HMundo/02_control_flujo/09_Ejercicio-calculadora.py
+++ Curso-python-HMundo/02_control_flujo/09_Ejercicio-calculadora.py
@@ -20,18 +20,21 @@ while True:
     n2 = input("Ingresa siguiente número: ")
     if n2.lower() == "salir":
         break
-    n2 = int(n2)
+    try: # -> se intenta convertir n2 a entero, si falla se convierte a float.
+        n2 = int(n2) # -> se intenta convertir n2 a entero.
+    except ValueError: # -> si falla la conversion a entero, y da error de valores incorrectos...
+        n2 = float(n2) # -> se convierte a float. En decimales por si quieres restar 0.xxx a un nuemero.
 
-    if op.lower() == "suma":
-        resultado += n2
-    elif op.lower() == "resta":
+    if op.lower() == "suma": # -> se usa lower() para que no importe si se escribe en mayusculas o minusculas y si es igual a suma.
+        resultado += n2  # -> se suma el resultado con n2 y se guarda en resultado.
+    elif op.lower() == "resta": 
         resultado -= n2
     elif op.lower() == "multi":
         resultado *= n2
     elif op.lower() == "div":
         resultado /= n2
     else:
-        print("Operación no válida")
+        print("Operación no válida") # -> si no es ninguna de las operaciones validas se indica que no es valida.
         break
 
-    print(f"El resultado es: {resultado}")
+    print(f"El resultado es: {resultado}") # -> se imprime el resultado de la operacion, que se haya elegido.
