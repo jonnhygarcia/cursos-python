7a15d93d7334f6053bb78b1f636143da9f09c9ad
jonnhygarcia <76399015+jonnhygarcia@users.noreply.github.com>
2025-10-24 17:09:07 +0200

    actualizando

--- Curso-Python/05_clases/01_introduccion.py
+++ Curso-Python/05_clases/01_introduccion.py
@@ -1,3 +1,12 @@
 # clases
 mensaje = "Hola Mundo"
 print(type(mensaje))
+
+# Clase es como un plano de construccion de una casa, para poder crear algo en un futuro, y para crear objetos
+# La clase ya construida seria un objeto una instancia de una clase.
+
+# Clases: el plano de construccion
+# objetos: la casa ya construida
+
+# Clase: Humano
+# Objeto: Nicolas, Felipe, Maria, Juan...
